ce99cc053a621b199ff6e5cdff4d0dc3346797f5
Anthony Ashcraft <anthonyashcraft91@gmail.com>
2023-01-28 11:54:57 -0500

    finished front page

--- server.js
+++ server.js
@@ -19,12 +19,13 @@ app.use(logger("dev"));
 app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "build")));
 
+app.use("/api/links", require("./routes/api/links"));
+
 app.use(require("./config/checkToken"));
 /*
 app.use('/api', routes) <====== Finish code once you got it
 */
 app.use("/api/users", require("./routes/api/users"));
-app.use("/api/links", require("./routes/api/links"));
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'build', 'index.html'))
--- src/pages/UserDashboard/UserDashboard.js
+++ src/pages/UserDashboard/UserDashboard.js
@@ -1,3 +1,26 @@
-export default function UserDashboard () {
-  return <>UserDashboard</>
+import { useState, useEffect } from "react";
+
+export default function UserDashboard({ user }) {
+  const [currentUserLinks, setCurrentUserLinks] = useState(null);
+
+  const getUserData = async () => {
+    try {
+      const response = await fetch(`api/users/${user._id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setCurrentUserLinks(data.links);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  return <>Hey, {user.name}! Your cat sucks.</>;
 }
